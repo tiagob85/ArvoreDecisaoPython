c7f072058805857b2ec8627fffd5edab09a068b6
tiagob85 <tiago.gpes@gmail.com>
2023-10-21 18:05:49 -0300

    Removendo comentários e código desnecessários

--- arvoredecisao.py
+++ arvoredecisao.py
@@ -17,86 +17,7 @@ from sklearn.metrics import classification_report, accuracy_score, confusion_mat
 from IPython.display import Image
 from sklearn.tree import export_graphviz
 from sklearn import tree
-'''
-# Lendo o dataset
-dataset = pd.read_csv('wine.data', header=None)
 
-dataset.columns = ['label'
-                   ,'alcohol',
-                   'malic_acid',
-                   'ash',
-                   'alcalinity_of_ash',
-                   'magnesium',
-                   'total_phenols',
-                   'flavanoids',
-                   'nonflavanoid_phenols',
-                   'proanthocyanins',
-                   'color_intensity',
-                   'hue',
-                   'OD280/OD315',
-                   'proline']
-
-dataset.head()
-
-# Divisão treino-teste
-
-x = dataset.values[:, 1:]
-y = dataset.values[:, 0]
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=0)
-
-#Feature Scaling
-scaler = StandardScaler()
-scaler.fit(x_train)
-
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
-
-
-# Treinamento
-def train_model(height):
-    model = DecisionTreeClassifier(criterion='entropy', max_depth=height, random_state=0)
-    model.fit(x_train, y_train)
-    return model
-
-# Avaliação do Modelo
-for height in range(1, 21):
-    model = train_model(height)
-    y_pred = model.predict(x_test)
-    
-    print('-----------------------------------------------\n')
-    print(f'Altura - {height}\n')
-    print("Precisão: "+ str(accuracy_score(y_test, y_pred)))
-    
-model = train_model(8)   
-
-tree.plot_tree(model)
-'''
-''' 
-feature_names = ['alcohol',
-                 'malic_acid',
-                 'ash',
-                 'alcalinity_of_ash',
-                 'magnesium',
-                 'total_phenols',
-                 'flavanoids',
-                 'nonflavanoid_phenols',
-                 'proanthocyanins',
-                 'color_intensity',
-                 'hue',
-                 'OD280/OD315',
-                 'proline']    
-    
-classes_names = ['%.f' % i for i in model.classes_]    
-
-dot_data = export_graphviz(model, filled=True, feature_names=feature_names, class_names=classes_names, rounded=True, special_characters=True)
-graph = pydotplus.graph_from_dot_data(dot_data)
-
-Image(graph.create_png())
-graph.write_png("tree.png")
-Image('tree.png')
-'''
-print("Arvore de decisão - Medium")
 
 class ArvoreDecisao:
    def __init__(self, dataset):
@@ -156,9 +77,7 @@ class ArvoreDecisao:
            
         self.model = self._train_model(8) 
         
-       # self._plot_tree()
 
-        #tree.plot_tree(self.model)
 
 arvoredecisao = ArvoreDecisao('wine.data')     
 
